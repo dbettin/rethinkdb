dfe718a538cd8e2b3fccf366155854d047e25501
Billy Welch <bwelch@42six.com>
2013-12-14 20:58:06 -0700

    Added some documentation

--- lib/rethinkdb.dart
+++ lib/rethinkdb.dart
@@ -18,35 +18,149 @@ part 'src/options.dart';
 
 
 // Connection Management
+/**
+ * Create a new connection to the database server. Accepts the following options:
+ * host: the host to connect to (default localhost).
+ * port: the port to connect on (default 28015).
+ * db: the default database (defaults to test).
+ * authKey: the authentication key (default none).
+ */
 Future<_Connection> connect({String db, String host: _Connection.DEFAULT_HOST, num port: _Connection.DEFAULT_PORT,
   String authKey: _Connection.DEFAULT_AUTH_KEY}) =>  new _Connection().connect(db,host,port,authKey);
 
-
+/**
+ *Reference a database.This command can be chained with other commands to do further processing on the data.
+ */
 _RqlDatabase db(String dbName,[Map options]) => new _RqlDatabase(dbName);
-_RqlDbCreate dbCreate(String dbName) => new _RqlDbCreate(dbName);
-_RqlDbDrop   dbDrop(String dbName) => new _RqlDbDrop(dbName);
-_RqlDbList   dbList() => new _RqlDbList();
 
+/**
+ * Create a database. A RethinkDB database is a collection of tables, similar to relational databases.
+ * If successful, the operation returns an object: {created: 1}. If a database with the same name already exists the operation throws RqlRuntimeError.
+ * Note: that you can only use alphanumeric characters and underscores for the database name.
+ */
+_RqlDbCreate dbCreate(String dbName) => new _RqlDbCreate(dbName);
+
+/**
+ * Drop a database. The database, all its tables, and corresponding data will be deleted.
+ * If successful, the operation returns the object {dropped: 1}.
+ * If the specified database doesn't exist a RqlRuntimeError is thrown.
+*/
+_RqlDbDrop dbDrop(String dbName) => new _RqlDbDrop(dbName);
+
+/**
+ * List all database names in the system. The result is a list of strings.
+ */
+_RqlDbList dbList() => new _RqlDbList();
+
+
+/**
+ * Select all documents in a table. This command can be chained with other commands to do further processing on the data.
+ */
 _RqlTable table(String tableName,[Map options]) => new _RqlTable(tableName,options);
+/**
+ * Create a table. A RethinkDB table is a collection of JSON documents.
+ * If successful, the operation returns an object: {created: 1}. If a table with the same name already exists, the operation throws RqlRuntimeError.
+ * Note: that you can only use alphanumeric characters and underscores for the table name.
+ */
 _RqlTableCreate tableCreate(String tableName,[Map options]) => new _RqlTableCreate(tableName,options);
+
+/**
+ * List all table names in a database. The result is a list of strings.
+ */
 _RqlTableList tableList() => new _RqlTableList();
+
+/**
+ * Drop a table. The table and all its data will be deleted.
+ */
 _RqlTableDrop tableDrop(String tableName,[Map options]) => new _RqlTableDrop(tableName,options);
 
+/**
+ * Specify ascending order on an attribute
+ */
 _RqlAsc asc(String attr) => new _RqlAsc(attr);
+
+/**
+ * Specify descending order on an attribute
+ */
 _RqlDesc desc(String attr) => new _RqlDesc(attr);
+
+/**
+ * Create a time object for a specific time.
+ */
 _RqlTime time(int year,int month,int day,String timezone,[int hour,int minute,num second]) => new _RqlTime(year, month, day,timezone,hour, minute, second);
+
+//TODO fix nativeTime
+/**
+ * Create a time object from a Dart DateTime object.
+ * Not working yet
+ */
 _RqlTime nativeTime(time) => new _RqlTime.nativeTime(time);
+
+/**
+ * Create a time object based on an iso8601 date-time string (e.g. '2013-01-01T01:01:01+00:00').
+ * We support all valid ISO 8601 formats except for week dates.
+ * If you pass an ISO 8601 date-time without a time zone, you must specify the time zone with the optarg default_timezone.
+ *
+ */
 _RqlISO8601 ISO8601(stringTime,[default_time_zone="Z"]) => new _RqlISO8601(stringTime,default_time_zone);
+
+/**
+ * Create a time object based on seconds since epoch.
+ *  The first argument is a double and will be rounded to three decimal places (millisecond-precision).
+ */
 _RqlEpochTime epochTime(eptime) => new _RqlEpochTime(eptime);
+
+/**
+ * Return a time object representing the current time in UTC.
+ *  The command now() is computed once when the server receives the query, so multiple instances of r.now() will always return the same time inside a query.
+ */
 _RqlNow now() => new _RqlNow();
 
+/**
+ * Evaluate the expr in the context of one or more value bindings.
+ * The type of the result is the type of the value returned from expr.
+ */
 _RqlDo reqlDo(arg,[args,expr]) => new _RqlDo(arg,[args],expr);
+
+/**
+ * If the test expression returns false or null, the false_branch will be executed.
+ * In the other cases, the true_branch is the one that will be evaluated.
+ * The branch command is effectively an if renamed due to language constraints.
+ * The type of the result is determined by the type of the branch that gets executed.
+ */
 _RqlBranch branch(test,trueBranch,falseBranch) => new _RqlBranch(test,trueBranch,falseBranch);
+
+/**
+ * Throw a runtime error. If called with no arguments inside the second argument to default, re-throw the current error.
+ */
 _RqlError error(String message) => new _RqlError(message);
+
+/**
+ * Create a javascript expression.
+ */
 _RqlJs js(String js) => new _RqlJs(js);
+
+/**
+ * Parse a JSON string on the server.
+ */
 _RqlJson json(String json) => new _RqlJson(json);
 
+/**
+ * Count the total size of the group.
+ */
 Map count = {"COUNT":true};
+
+/**
+ * Compute the sum of the given field in the group.
+ */
 Map sum(String attr) => {'SUM':attr};
+
+/**
+ * Compute the average value of the given attribute for the group.
+ */
 Map avg(String attr) => {"AVG": attr};
+
+/**
+ * Returns the currently visited document.
+ */
 _RqlImplicitVar row(String rowName) => new _RqlImplicitVar();
